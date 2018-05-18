ffebe5844a2d6d71423c3993e2cb9e2f8bc4ecfe
jipenfei <921374010@qq.com>
2018-05-18 19:36:02 +0800

    网站

---
+++ js/contain.js
@@ -0,0 +1,19 @@
+/**
+ * Created by ASUS-PC on 2018/5/18.
+ */
+var c=document.getElementById("myCanvas");
+aa(18,15,z="90%");
+var c=document.getElementById("myCanvas2");
+aa(18,12,z="80%");
+var c=document.getElementById("myCanvas3");
+aa(18,12)
+var c=document.getElementById("myCanvas4");
+aa(18,10,z="60%")
+//   画布结束
+
+function load() {
+    setInterval(function(){ A(); }, 2000);
+}
+function A(){
+    $(".glyphicon-chevron-right").trigger("click")
+}
